58b0ad21f26d0d725467415a0b0db89a26d6c1c0
Maminiainaando <hei.maminiaina.444@gmail.com>
2024-03-15 10:22:38 +0300

    constraint setting and table updating

--- script.sql
+++ script.sql
@@ -1,32 +1,33 @@
-CREATE TABLE account(
-        id   int primary key,
-        name     varchar (100),
-        lastname varchar (50),
-        birthdate Date,
-        balance  Float,
-        bank_type varchar (50)
+CREATE TABLE account (
+    id_account   serial PRIMARY KEY,
+    name      VARCHAR(100) not null,
+    lastname  VARCHAR(50) not null,
+    birthdate DATE CHECK (birthdate > (CURRENT_DATE - INTERVAL '21 years')),
+    balance   FLOAT,
+    account_number varchar (50),
+    bank_type VARCHAR(50) not null
 );
- CREATE TABLE balance(
-        id int primary key,
-        balance float ,
-        date_reference date,
-        loans float,
-        interset_loans float
- );
+CREATE TABLE transfer_history(
+        id_transfer serial primary key,
+        id_send int references account(id_account),
+        id_receive int references account(id_account),
+        date_registration date
+);
 CREATE TABLE transaction(
-        id int primary key,
+        id_transaction serial primary key,
         label varchar (100),
+        transaction_amount float,
+        bank_type varchar (50),
         date_effect date,
         date_registration date,
+        transaction_type varchar (50),---credit ou debit---
+        id_account int references account(id_account),
+        id_transfer int references transfer_history(id_transfer),
         status varchar (50),
-        bank_type varchar (50)
 );
-CREATE TABLE account_statement(
-        id_account int references account(id),
-        id int primary key,
-        date date,
-        pattern varchar (100),
-        credit float,
-        flow float,
-        balance float
+
+CREATE TABLE transaction_history(
+       last_balance float ,
+       date_registration date,
+       id_account int references account(id_account)   
 );
